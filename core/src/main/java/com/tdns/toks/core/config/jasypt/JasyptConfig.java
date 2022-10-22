714b8bb063d147c0503438d7e065682cc384cbae
SunwooHo <kkannu0407@gmail.com>
2022-10-22 17:57:50 +0900

    인프라 세팅 및 CICD 작업 (#5)
    * feat: cicd 스크립트 작성
    
    * feat: jasypt 적용
    
    * feat: jasypt 설정 추가
    
    * feat: jib 설정 추가

---
+++ core/src/main/java/com/tdns/toks/core/config/jasypt/JasyptConfig.java
@@ -0,0 +1,10 @@
+package com.tdns.toks.core.config.jasypt;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEncryptableProperties
+public class JasyptConfig {
+}
