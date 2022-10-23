10d0ccf816e89a5b596124cd154a59885361e203
SunwooHo <kkannu0407@gmail.com>
2022-10-23 12:30:01 +0900

    JIB 실행 명령어 모듈 이름 추가 (#7)
    * fix: querydsl 설정 추가
    
    * fix: jib 실행 모듈 이름 추가

--- core/src/main/java/com/tdns/toks/core/common/model/dto/ResponseDto.java
+++ core/src/main/java/com/tdns/toks/core/common/model/dto/ResponseDto.java
@@ -11,7 +11,6 @@ import java.io.Serializable;
 
 @Data
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@RequiredArgsConstructor
 public class ResponseDto<T> implements Serializable {
     private T data;
 
