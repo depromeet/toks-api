2d63e62c61ee52a9265e471e16ce5f9d9b48343b
sieun <sieun960513@naver.com>
2022-11-17 21:06:39 +0900

    :pencil2: 간격 및 불필요 로그 제거

--- core/src/main/java/com/tdns/toks/core/common/security/oauth/CustomOAuth2UserService.java
+++ core/src/main/java/com/tdns/toks/core/common/security/oauth/CustomOAuth2UserService.java
@@ -16,7 +16,6 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
@@ -28,18 +27,11 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
     @Transactional
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService = new DefaultOAuth2UserService();
-
         OAuth2User oAuth2User = oAuth2UserService.loadUser(userRequest);
-        log.info("[CustomOAuth2UserService]");
-        log.info(oAuth2User.toString());
-
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
-
         OAuth2Attribute oAuth2Attribute = OAuth2Attribute.of(registrationId, oAuth2User.getAttributes());
         User user = userRepository.findByEmail(oAuth2Attribute.getEmail())
                                     .orElseGet(() -> createUser(oAuth2Attribute));
-
-
         return new UserDetailDTO(user, oAuth2Attribute.getAttributes(), jwtTokenProvider.generateToken(oAuth2Attribute.getEmail()));
     }
 
