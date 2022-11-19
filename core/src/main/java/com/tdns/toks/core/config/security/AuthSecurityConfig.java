692cec757ef0eb2535c92f2f0c2db52be0a9483f
sieun <sieun960513@naver.com>
2022-11-19 10:21:00 +0900

    :recycle: 사용 메서드 단순화

--- core/src/main/java/com/tdns/toks/core/config/security/AuthSecurityConfig.java
+++ core/src/main/java/com/tdns/toks/core/config/security/AuthSecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -53,13 +52,7 @@ public class AuthSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                     .antMatchers(permitUrl).permitAll()
-                    .antMatchers(HttpMethod.GET, "/**").authenticated()
-                    .antMatchers(HttpMethod.POST, "/**").authenticated()
-                    .antMatchers(HttpMethod.DELETE, "/**").authenticated()
-                    .antMatchers(HttpMethod.PUT, "/**").authenticated()
-                    .antMatchers(HttpMethod.PATCH, "/**").authenticated()
-                    .anyRequest().permitAll()
-
+                    .anyRequest().authenticated()
                 .and()
                 .oauth2Login()
                     .loginPage("/login/social") // 추후 프론트 url 맞춰야 함
