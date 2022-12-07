edf50bd21d34991c94080435afac347f90b873ed
hocaron <kkannu0407@gmail.com>
2022-12-07 20:53:40 +0900

    :recycle: 리뷰 반영

--- api/src/main/java/com/tdns/toks/api/domain/ranking/service/RankingApiService.java
+++ api/src/main/java/com/tdns/toks/api/domain/ranking/service/RankingApiService.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.tdns.toks.core.domain.ranking.service.RankingService;
-import com.tdns.toks.core.domain.study.model.entity.Study;
 import com.tdns.toks.core.domain.study.service.StudyService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,7 +19,7 @@ public class RankingApiService {
 	private final RankingService rankingService;
 
 	public RankingsApiResponse getByStudyId(final Long studyId) {
-		Study study = studyService.getOrThrow(studyId);
+		var study = studyService.getOrThrow(studyId);
 		return new RankingsApiResponse(rankingService.getByStudyId(study.getId()));
 	}
 }
