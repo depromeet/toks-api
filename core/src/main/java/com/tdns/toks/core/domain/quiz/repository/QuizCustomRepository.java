068bebc016f621c1bc2f97e0789643ef5db3b5b3
hocaron <kkannu0407@gmail.com>
2022-12-11 16:11:16 +0900

    :sparkles: 스터디 id로 퀴즈 조회 쿼리 생성

--- core/src/main/java/com/tdns/toks/core/domain/quiz/repository/QuizCustomRepository.java
+++ core/src/main/java/com/tdns/toks/core/domain/quiz/repository/QuizCustomRepository.java
@@ -1,9 +1,12 @@
 package com.tdns.toks.core.domain.quiz.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.tdns.toks.core.domain.quiz.model.dto.QuizSimpleDTO;
 
 public interface QuizCustomRepository {
 	Optional<QuizSimpleDTO> retrieveById(Long id);
+
+	List<QuizSimpleDTO> retrieveByStudyId(Long studyId);
 }
