4e6524a2000ee65253c976d29184afbc9ffaf059
sieun <sieun960513@naver.com>
2022-12-12 23:48:33 +0900

    :fire: rank 에서 quizRank로 변경

--- core/src/main/java/com/tdns/toks/core/domain/rank/repository/RankingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tdns.toks.core.domain.rank.repository;
-
-import com.tdns.toks.core.domain.rank.model.entity.Ranking;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RankingRepository extends JpaRepository<Ranking, Long> {
-}
