514e22f77a4a54f5f34c6e0dea9e42954ab52168
south-daria <jy.nam@weversecompany.com>
2022-12-16 05:07:19 +0900

    #57 security filter skip 로직 추가

--- core/src/main/java/com/tdns/toks/core/common/filter/JwtAuthenticationFilter.java
+++ core/src/main/java/com/tdns/toks/core/common/filter/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.tdns.toks.core.common.filter;
 
+import com.tdns.toks.core.common.exception.ApplicationErrorException;
 import com.tdns.toks.core.common.exception.ApplicationErrorType;
 import com.tdns.toks.core.common.exception.SilentApplicationErrorException;
 import com.tdns.toks.core.common.security.JwtTokenProvider;
@@ -7,11 +8,14 @@ import com.tdns.toks.core.domain.user.model.dto.UserDTO;
 import com.tdns.toks.core.domain.user.model.entity.User;
 import com.tdns.toks.core.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -19,11 +23,17 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Value("#{'${security.filter-skip}' .split(',')}")
+    private final List<String> filterSkipUrl;
+
+    @Value("${security.permit-url}")
+    private final String[] permitUrl;
 
     private final JwtTokenProvider jwtTokenProvider; // JWT 토큰을 생성 및 검증 모듈 클래스
     private final UserService userService;
@@ -37,6 +47,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = jwtTokenProvider.getAuthToken(request);
 
+        if (!Arrays.stream(permitUrl).anyMatch(fl -> request.getServletPath().contains(fl)) && StringUtils.isEmpty(token)) {
+            throw new ApplicationErrorException(ApplicationErrorType.INVALID_ACCESS_TOKEN);
+        }
+
         if (token != null) {
             if (jwtTokenProvider.verifyToken(token)) {
                 String email = jwtTokenProvider.getUid(token);
@@ -44,7 +58,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 Authentication auth = getAuthentication(UserDTO.convertEntityToDTO(user));
                 SecurityContextHolder.getContext().setAuthentication(auth);
             } else {
-                throw new SilentApplicationErrorException(ApplicationErrorType.INVALID_ACCESS_TOKEN);
+                throw new SilentApplicationErrorException(ApplicationErrorType.INVALID_LOGIN_INFO);
             }
         }
         filterChain.doFilter(request, response);
@@ -56,6 +70,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 "",
                 List.of(new SimpleGrantedAuthority(user.getUserRole().getAuthority()))
         );
+    }
+
+    @Override
+    // Auth Filter 안태우는 URL 추가
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        return filterSkipUrl.stream()
+                .anyMatch(ex -> request.getServletPath().contains(ex));
     }
 }
 
