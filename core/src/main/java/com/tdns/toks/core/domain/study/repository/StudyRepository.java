d85bb3516c30040044eb818378ff85240272200d
sieun <sieun960513@naver.com>
2022-12-16 15:22:41 +0900

    :recycle: nativeQuery 명시

--- core/src/main/java/com/tdns/toks/core/domain/study/repository/StudyRepository.java
+++ core/src/main/java/com/tdns/toks/core/domain/study/repository/StudyRepository.java
@@ -11,6 +11,6 @@ import java.util.List;
 @Repository
 public interface StudyRepository extends JpaRepository<Study, Long> {
 
-    @Query("select s from Study s join QuizRank r on r.studyId = s.id where r.userId =:userId and s.status = 'IN_PROGRESS'")
+    @Query(value = "select s from Study s join QuizRank r on r.studyId = s.id where r.userId =:userId and s.status = 'IN_PROGRESS'", nativeQuery = true)
     List<Study> getAllInProgressStudyByUserId(@Param("userId") Long userId);
 }
