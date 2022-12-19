862b6e0c5ddee4fba9633f682d33fb6cffba4d5b
hocaron <kkannu0407@gmail.com>
2022-12-20 03:05:31 +0900

    :bug: 스웨거 버그 수정

--- api/src/main/java/com/tdns/toks/api/domain/quiz/model/dto/QuizReplyHistoryApiDTO.java
+++ api/src/main/java/com/tdns/toks/api/domain/quiz/model/dto/QuizReplyHistoryApiDTO.java
@@ -8,32 +8,34 @@ import com.tdns.toks.core.domain.quiz.model.dto.QuizReplyHistoryDto;
 import com.tdns.toks.core.domain.quiz.model.entity.QuizReplyHistory;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 public class QuizReplyHistoryApiDTO {
 	@Getter
+	@AllArgsConstructor
 	@Schema(name = "QuizReplyHistoryRequest", description = "QUIZ REPLY HISTORY 생성 요청 모델")
 	public static class QuizReplyHistoryRequest {
 		@NotEmpty(message = "퀴즈 답변은 필수 항목입니다.")
 		@Schema(accessMode = Schema.AccessMode.READ_WRITE, required = true, name = "answer", description = "댭변")
 		private String answer;
 
-		@Schema(accessMode = Schema.AccessMode.READ_WRITE, required = true, name = "quiz id", description = "quiz id")
+		@Schema(accessMode = Schema.AccessMode.READ_WRITE, required = true, name = "quizId", description = "quiz id")
 		private Long quizId;
 	}
 
 	@Getter
 	@RequiredArgsConstructor
-	@Schema(name = "QuizReplyHistoryRequest", description = "QUIZ REPLY HISTORY 생성 응답 모델")
+	@Schema(name = "QuizReplyHistoryResponse", description = "QUIZ REPLY HISTORY 생성 응답 모델")
 	public static class QuizReplyHistoryResponse {
-		@Schema(accessMode = Schema.AccessMode.READ_WRITE, required = true, name = "quiz reply history id", description = "quiz reply history id")
+		@Schema(accessMode = Schema.AccessMode.READ_WRITE, required = true, name = "quizReplyHistoryId", description = "quiz reply history id")
 		private final Long quizReplyHistoryId;
 
 		@Schema(accessMode = Schema.AccessMode.READ_WRITE, required = true, name = "answer", description = "댭변")
 		private final String answer;
 
-		@Schema(accessMode = Schema.AccessMode.READ_WRITE, required = true, name = "quiz id", description = "quiz id")
+		@Schema(accessMode = Schema.AccessMode.READ_WRITE, required = true, name = "quizId", description = "quiz id")
 		private final Long quizId;
 
 		public static QuizReplyHistoryResponse toResponse(QuizReplyHistory quizReplyHistory) {
@@ -49,7 +51,7 @@ public class QuizReplyHistoryApiDTO {
 	@RequiredArgsConstructor
 	@Schema(name = "QuizReplyHistoriesResponse", description = "QUIZ REPLY HISTORY 생성 응답 모델")
 	public static class QuizReplyHistoriesResponse {
-		@Schema(accessMode = Schema.AccessMode.READ_WRITE, required = true, name = "quiz reply history 리스트", description = "quiz reply history 리스트")
+		@Schema(accessMode = Schema.AccessMode.READ_WRITE, required = true, name = "quizReplyHistories", description = "quizReplyHistories")
 		private final List<QuizReplyHistoryDto> quizReplyHistories;
 	}
 }
