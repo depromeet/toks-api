21a0f8f94f2c343314e7e2b77c09d7f4c4623b98
SunwooHo <kkannu0407@gmail.com>
2022-12-19 23:21:45 +0900

    #10 요청 / 응답 로깅 (#64)
    * :sparkles: 로깅 필터 추가
    
    * :sparkles: 로그 추가
    
    * :sparkles: 메서드 분리
    
    * :sparkles: 메서드 분리
    
    * :art: status code 추가
    
    * :bug: cors 에러 해결

---
+++ api/src/main/java/com/tdns/toks/api/filter/LoggingFilter.java
@@ -0,0 +1,85 @@
+package com.tdns.toks.api.filter;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class LoggingFilter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+		ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+		ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+		filterChain.doFilter(requestWrapper, responseWrapper);
+
+		log.info("\n[Request] {} \n"
+				+ "[Request Headers] {} \n"
+				+ "[Request Body] {} \n"
+				+ "[Status Code] {} \n"
+				+ "[Response Body] {}",
+			getRequest(request),
+			getHeaders(request),
+			contentBody(requestWrapper.getContentAsByteArray()),
+			response.getStatus(),
+			contentBody(responseWrapper.getContentAsByteArray())
+		);
+
+		responseWrapper.copyBodyToResponse();
+	}
+
+	private String getRequest(HttpServletRequest request) {
+		String result = "";
+		result = request.getMethod() + " " + request.getRequestURI();
+		result += request.getQueryString() != null ? "?" + request.getQueryString() : "";
+		return result;
+	}
+
+	private String getHeaders(HttpServletRequest request) {
+		Map<String, String> headerMap = new HashMap<>();
+
+		Enumeration<String> headerArray = request.getHeaderNames();
+		while (headerArray.hasMoreElements()) {
+			String headerName = headerArray.nextElement();
+			headerMap.put(headerName, request.getHeader(headerName));
+		}
+
+		ObjectMapper mapper = new ObjectMapper();
+		String json = "";
+		try {
+			json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(headerMap);
+		} catch (IOException e) {
+			log.error("[JSON PARSE ERROR] {}", e.getMessage(), e);
+		}
+		return json;
+	}
+
+	private String contentBody(final byte[] contents) {
+		ObjectMapper mapper = new ObjectMapper();
+		String json = "";
+		try {
+			Map map = mapper.readValue(new String(contents), Map.class);
+			json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
+		} catch (IOException e) {
+			log.error("[JSON PARSE ERROR] {}", e.getMessage(), e);
+		}
+		return json;
+	}
+}
