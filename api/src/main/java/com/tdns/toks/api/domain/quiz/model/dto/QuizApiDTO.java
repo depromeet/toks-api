6df00194cc5274c826f592bba5b6f04069a9c713
hocaron <kkannu0407@gmail.com>
2022-12-31 22:31:18 +0100

    :recycle: validated 조건 추가

--- api/src/main/java/com/tdns/toks/api/domain/quiz/model/dto/QuizApiDTO.java
+++ api/src/main/java/com/tdns/toks/api/domain/quiz/model/dto/QuizApiDTO.java
@@ -5,6 +5,8 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import javax.validation.constraints.NotNull;
+
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -59,6 +61,9 @@ public class QuizApiDTO {
 		@Schema(accessMode = Schema.AccessMode.READ_WRITE, required = true, name = "studyId", description = "study id")
 		private final Long studyId;
 
+		@Schema(accessMode = Schema.AccessMode.READ_WRITE, required = true, name = "quizRound", description = "quiz round")
+		private final Long quizRound;
+
 		public static QuizSimpleResponse toResponse(QuizSimpleDTO quizSimpleDTO) {
 			return new QuizSimpleResponse(
 				quizSimpleDTO.getQuizId(),
@@ -71,7 +76,8 @@ public class QuizApiDTO {
 				new Timestamp(System.currentTimeMillis()),
 				quizSimpleDTO.getImageUrls(),
 				quizSimpleDTO.getCreator(),
-				quizSimpleDTO.getStudyId()
+				quizSimpleDTO.getStudyId(),
+				quizSimpleDTO.getQuizRound()
 			);
 		}
 	}
@@ -117,6 +123,9 @@ public class QuizApiDTO {
 		@Schema(accessMode = Schema.AccessMode.READ_WRITE, required = true, name = "quizStatus", description = "study id")
 		private final QuizStatusType quizStatus;
 
+		@Schema(accessMode = Schema.AccessMode.READ_WRITE, required = true, name = "quizRound", description = "quiz round")
+		private final Long quizRound;
+
 		public static QuizResponse toResponse(QuizSimpleDTO quizSimpleDTO, List<UserSimpleDTO> unSubmitter, QuizStatusType quizStatus) {
 			return new QuizResponse(
 				quizSimpleDTO.getQuizId(),
@@ -130,7 +139,8 @@ public class QuizApiDTO {
 				quizSimpleDTO.getCreator(),
 				unSubmitter,
 				quizSimpleDTO.getStudyId(),
-				quizStatus
+				quizStatus,
+				quizSimpleDTO.getQuizRound()
 			);
 		}
 	}
@@ -146,26 +156,33 @@ public class QuizApiDTO {
 	@Data
 	@Schema(name = "QuizRequest", description = "QUIZ REPLY HISTORY 생성 요청 모델")
 	public static class QuizRequest {
+		@NotNull(message = "퀴즈 답변은 필수 항목입니다.")
 		@Schema(accessMode = Schema.AccessMode.READ_WRITE, required = true, name = "quiz", description = "quiz")
 		private String quiz;
 
+		@NotNull(message = "퀴즈 타입은 필수 항목입니다.")
 		@Schema(accessMode = Schema.AccessMode.READ_WRITE, required = true, name = "quizType", description = "quizType")
 		private QuizType quizType;
 
+		@NotNull(message = "퀴즈 설명은 필수 항목입니다.")
 		@Schema(accessMode = Schema.AccessMode.READ_WRITE, required = true, name = "description", description = "description")
 		private String description;
 
+		@NotNull(message = "퀴즈 답변은 필수 항목입니다.")
 		@Schema(accessMode = Schema.AccessMode.READ_WRITE, required = true, name = "answer", description = "answer")
 		private String answer;
 
+		@NotNull(message = "퀴즈 시작시간은 필수 항목입니다.")
 		@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
 		@Schema(accessMode = Schema.AccessMode.READ_WRITE, required = true, name = "startedAt", description = "startedAt")
 		private LocalDateTime startedAt;
 
+		@NotNull(message = "퀴즈 종료시간은 필수 항목입니다.")
 		@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
 		@Schema(accessMode = Schema.AccessMode.READ_WRITE, required = true, name = "endedAt", description = "endedAt")
 		private LocalDateTime endedAt;
 
+		@NotNull(message = "study id는 필수 항목입니다.")
 		@Schema(accessMode = Schema.AccessMode.READ_WRITE, required = true, name = "studyId", description = "studyId")
 		private Long studyId;
 
