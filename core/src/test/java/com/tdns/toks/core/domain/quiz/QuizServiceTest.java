eda6a8d4a7457df277373b6b7601eecb2e646922
hocaron <kkannu0407@gmail.com>
2023-01-10 00:21:13 +0900

    :white_check_mark: 사용하지 않는 서비스 삭제

--- core/src/test/java/com/tdns/toks/core/domain/quiz/QuizServiceTest.java
+++ core/src/test/java/com/tdns/toks/core/domain/quiz/QuizServiceTest.java
@@ -1,24 +1,23 @@
 package com.tdns.toks.core.domain.quiz;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import com.tdns.toks.core.common.exception.SilentApplicationErrorException;
 import com.tdns.toks.core.domain.quiz.repository.QuizReplyHistoryRepository;
 import com.tdns.toks.core.domain.quiz.repository.QuizRepository;
 import com.tdns.toks.core.domain.quiz.service.QuizService;
-import com.tdns.toks.core.domain.study.repository.StudyRepository;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.*;
 
 public class QuizServiceTest {
 	private final QuizRepository quizRepository = mock(QuizRepository.class);
 	private final QuizReplyHistoryRepository quizReplyHistoryRepository  = mock(QuizReplyHistoryRepository.class);
-	private final StudyRepository studyRepository = mock(StudyRepository.class);
 
-	private final QuizService sut = new QuizService(studyRepository, quizRepository, quizReplyHistoryRepository);
+	private final QuizService sut = new QuizService(quizRepository, quizReplyHistoryRepository);
 
 	@Test
 	@DisplayName("퀴즈가 없으면 SilentApplicationErrorException 던진다")
