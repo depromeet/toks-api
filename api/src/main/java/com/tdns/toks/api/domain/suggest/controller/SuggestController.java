e4436e047c87dfedf88862abf4ef8b2acbc4dae0
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-02-11 03:23:49 +0900

    fix: 잘못된 매핑 수정

--- api/src/main/java/com/tdns/toks/api/domain/suggest/controller/SuggestController.java
+++ api/src/main/java/com/tdns/toks/api/domain/suggest/controller/SuggestController.java
@@ -8,7 +8,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +21,7 @@ public class SuggestController {
     private final SuggestApiService suggestApiService;
 
     @Operation(summary = "똑스에게 제안하기")
-    @GetMapping
+    @PostMapping
     public ResponseEntity<?> register(
             @RequestBody SuggestRegisterRequest request
     ) {
