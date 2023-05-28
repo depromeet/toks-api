222a8b57c31ff24a6a23f199eefa4c78fa1ff7fd
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-05-29 03:58:20 +0900

    feat: quiz 조회 query

--- core/src/main/java/com/tdns/toks/core/domain/quiz/repository/QuizRepository.java
+++ core/src/main/java/com/tdns/toks/core/domain/quiz/repository/QuizRepository.java
@@ -1,10 +1,13 @@
 package com.tdns.toks.core.domain.quiz.repository;
 
 import com.tdns.toks.core.domain.quiz.model.entity.Quiz;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -17,4 +20,7 @@ public interface QuizRepository extends JpaRepository<Quiz, Long>, QuizCustomRep
 
     @Transactional(readOnly = true)
     Boolean existsByStudyIdAndRound(Long studyId, Integer round);
+
+    @Transactional(readOnly = true)
+    Page<Quiz> findAllByCategoryIdIn(List<String> categoryId, Pageable pageable);
 }
