82db20683a65a0f81cecc6b2b77a6029f11c4df2
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-06-04 13:44:00 +0900

    chore: open api config 수정

--- api/src/main/java/com/tdns/toks/api/config/OpenApiConfig.java
+++ api/src/main/java/com/tdns/toks/api/config/OpenApiConfig.java
@@ -1,11 +1,5 @@
 package com.tdns.toks.api.config;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -15,6 +9,11 @@ import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 import static com.tdns.toks.core.common.security.Constants.TOKS_AUTH_HEADER_KEY;
 
@@ -24,23 +23,23 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI openAPI(@Value("${springdoc.version}") String appVersion) {
         return new OpenAPI()
-            .addServersItem(new Server().url("/"))
-            .components(new Components().addSecuritySchemes(TOKS_AUTH_HEADER_KEY,
-                new SecurityScheme()
-                    .type(SecurityScheme.Type.APIKEY)
-                    .in(SecurityScheme.In.HEADER)
-                    .name(TOKS_AUTH_HEADER_KEY)
-                    .description("인증이 필요한 경우 ex) bearer xxxxxxx")
-            ))
-            .security(List.of(new SecurityRequirement().addList(TOKS_AUTH_HEADER_KEY)))
-            .externalDocs(new ExternalDocumentation())
-            .info(new Info()
-                    .title("TOKS API Documents")
-                    .description("TOKS Public APIs For Clients.")
-                    .contact(new Contact().name("Contact 오개안말"))
-                    .version(appVersion)
-                .license(new License().name("TDNS. All rights reserved.").url("https://weverse.co/"))
-            );
+                .addServersItem(new Server().url("/"))
+                .components(new Components().addSecuritySchemes(TOKS_AUTH_HEADER_KEY,
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.HEADER)
+                                .name(TOKS_AUTH_HEADER_KEY)
+                                .description("인증이 필요한 경우 ex) X-TOKS-AUTH-TOKEN xxxxxxx")
+                ))
+                .security(List.of(new SecurityRequirement().addList(TOKS_AUTH_HEADER_KEY)))
+                .externalDocs(new ExternalDocumentation())
+                .info(new Info()
+                        .title("TOKS API Documents")
+                        .description("TOKS Public APIs For Clients.")
+                        .contact(new Contact().name("Contact 오개안말"))
+                        .version(appVersion)
+                        .license(new License().name("TDNS. All rights reserved.").url("https://tokstudy.com/"))
+                );
 
     }
 
