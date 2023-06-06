9669c336b1199e6312993fb76b7582f32d17ee7e
sieun <sieun960513@naver.com>
2023-06-06 16:07:33 +0900

    :sparkles: v2 API 퀴즈 부분 변경 및 테스트용 컨트롤러

--- api/src/main/java/com/tdns/toks/api/domain/quiz/controller/QuizCommentController.java
+++ api/src/main/java/com/tdns/toks/api/domain/quiz/controller/QuizCommentController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "퀴즈 댓글 관리", description = "QUIZ Comment API")
 @RestController
-@RequestMapping(path = "/api/v1", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path = "/api/v2", produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 public class QuizCommentController {
     private final QuizCommentService quizCommentService;
+
+    @Operation(summary = "api v2 테스트", description = "permit all 확인용")
+    @GetMapping("/quiz/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("testHere");
+    }
 
     @Operation(summary = "댓글 다건 조회", description = "내림차순으로 제공")
     @GetMapping("/quizzes/{quizId}/comments")
