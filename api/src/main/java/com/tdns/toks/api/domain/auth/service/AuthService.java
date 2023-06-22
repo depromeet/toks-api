a5bb0901dc6c8551d6898f14c3baed8f287af9a5
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-06-22 21:26:30 +0900

    fix: legacy auth domain 이관

--- api/src/main/java/com/tdns/toks/api/domain/auth/service/AuthService.java
+++ api/src/main/java/com/tdns/toks/api/domain/auth/service/AuthService.java
@@ -3,6 +3,7 @@ package com.tdns.toks.api.domain.auth.service;
 import com.tdns.toks.api.domain.user.model.dto.UserApiDTO;
 import com.tdns.toks.core.common.exception.ApplicationErrorType;
 import com.tdns.toks.core.common.exception.SilentApplicationErrorException;
+import com.tdns.toks.core.common.security.JwtTokenProvider;
 import com.tdns.toks.core.domain.auth.model.AuthUser;
 import com.tdns.toks.core.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AuthService {
     private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
 
     public UserApiDTO.UserInfoResponse getMyInfos(AuthUser authUser) {
         var user = userRepository.findById(authUser.getId())
@@ -23,5 +25,36 @@ public class AuthService {
                 user.getThumbnailImageUrl(),
                 user.getProfileImageUrl()
         );
+    }
+
+    public UserApiDTO.UserRenewAccessTokenResponse renewAccessToken(
+            AuthUser authUser,
+            UserApiDTO.UserRenewAccessTokenRequest request
+    ) {
+        var requestRefreshToken = request.getRefreshToken();
+
+        if (!jwtTokenProvider.verifyToken(requestRefreshToken)) {
+            throw new SilentApplicationErrorException(ApplicationErrorType.INVALID_REFRESH_TOKEN);
+        }
+
+        var email = jwtTokenProvider.getUid(requestRefreshToken);
+
+        var user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new SilentApplicationErrorException(ApplicationErrorType.UNKNOWN_USER));
+
+        var userRefreshToken = user.getRefreshToken();
+        if (!requestRefreshToken.equals(userRefreshToken)) {
+            throw new SilentApplicationErrorException(ApplicationErrorType.INVALID_REFRESH_TOKEN);
+        }
+
+        var accessToken = jwtTokenProvider.renewAccessToken(user.getEmail());
+
+        return new UserApiDTO.UserRenewAccessTokenResponse(accessToken);
+    }
+
+    public void deleteRefreshToken(AuthUser authUser) {
+        var user = userRepository.findById(authUser.getId())
+                .orElseThrow(() -> new SilentApplicationErrorException(ApplicationErrorType.NOT_FOUND_USER));
+        user.setRefreshToken("logout");
     }
 }
