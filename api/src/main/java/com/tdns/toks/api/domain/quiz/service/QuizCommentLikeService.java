2aacf78e72b949411f94b3fff820099a89686f1e
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-07-11 01:25:27 +0900

    fix: quiz Comment Like cache 처리 수정

--- api/src/main/java/com/tdns/toks/api/domain/quiz/service/QuizCommentLikeService.java
+++ api/src/main/java/com/tdns/toks/api/domain/quiz/service/QuizCommentLikeService.java
@@ -1,23 +1,18 @@
 package com.tdns.toks.api.domain.quiz.service;
 
+import com.tdns.toks.api.cache.CacheFactory;
+import com.tdns.toks.api.cache.CacheService;
 import com.tdns.toks.core.domain.auth.model.AuthUser;
 import com.tdns.toks.core.domain.quizcomment.model.entity.QuizCommentLike;
 import com.tdns.toks.core.domain.quizcomment.repository.QuizCommentLikeRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-
-import java.util.concurrent.CompletableFuture;
 
 @Service
 @RequiredArgsConstructor
 public class QuizCommentLikeService {
     private final QuizCommentLikeRepository quizCommentLikeRepository;
-    // TODO : 분리
-    private final StringRedisTemplate redisTemplate;
-
-    public final static String QUIZ_COMMENT_LIKE_CACHE = "quiz:comment:like:";
+    private final CacheService cacheService;
 
     public void like(AuthUser authUser, Long commendId) {
         if (!quizCommentLikeRepository.existsByCommentIdAndUid(commendId, authUser.getId())) {
@@ -35,15 +30,13 @@ public class QuizCommentLikeService {
                 .ifPresent(commentLike -> quizCommentLikeRepository.deleteById(commentLike.getId()));
     }
 
-    public int count(long quizId) {
-        var count = redisTemplate.opsForValue().get(
-                QUIZ_COMMENT_LIKE_CACHE + quizId
-        );
+    public int count(long commentId) {
+        var count = cacheService.getOrNull(CacheFactory.makeCachedQuizCommentCount(commentId));
 
         if (count == null) {
             return 0;
         }
 
-        return Integer.parseInt(count);
+        return count;
     }
 }
