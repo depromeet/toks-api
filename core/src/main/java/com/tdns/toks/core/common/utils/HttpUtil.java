80c98dd7115235922dba5ecf801dc99d4a9c7447
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-07-17 00:46:27 +0900

    fix: get client Ip Address

--- core/src/main/java/com/tdns/toks/core/common/utils/HttpUtil.java
+++ core/src/main/java/com/tdns/toks/core/common/utils/HttpUtil.java
@@ -1,8 +1,6 @@
 package com.tdns.toks.core.common.utils;
 
 import javax.servlet.http.HttpServletRequest;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.Arrays;
 
 public class HttpUtil {
@@ -15,21 +13,10 @@ public class HttpUtil {
                 "HTTP_X_FORWARDED_FOR"
         };
 
-        var ipAddress = Arrays.stream(headers)
+        return Arrays.stream(headers)
                 .map(request::getHeader)
                 .filter(ip -> ip != null && !ip.isEmpty() && !"unknown".equalsIgnoreCase(ip))
                 .findFirst()
                 .orElse(request.getRemoteAddr());
-
-        try {
-            var inetAddress = InetAddress.getByName(ipAddress);
-            if (inetAddress.getHostAddress().contains(":")) {
-                return inetAddress.getHostAddress().split(":")[0];
-            } else {
-                return inetAddress.getHostAddress();
-            }
-        } catch (UnknownHostException e) {
-            return null;
-        }
     }
 }
