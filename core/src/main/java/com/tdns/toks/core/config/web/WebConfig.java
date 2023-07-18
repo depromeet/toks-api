4787b973a1e8a108a3dabfc5d7c0d1e8f9006116
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-07-18 16:44:32 +0900

    fix: cors origins

--- core/src/main/java/com/tdns/toks/core/config/web/WebConfig.java
+++ core/src/main/java/com/tdns/toks/core/config/web/WebConfig.java
@@ -2,7 +2,11 @@ package com.tdns.toks.core.config.web;
 
 import com.tdns.toks.core.common.security.resolver.UserArgumentResolver;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +20,22 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(userArgumentResolver);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(UrlBasedCorsConfigurationSource.class)
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        var corsConfig = new CorsConfiguration();
+
+        corsConfig.addAllowedOriginPattern(CorsConfiguration.ALL);
+        corsConfig.addAllowedHeader(CorsConfiguration.ALL);
+        corsConfig.addAllowedMethod(CorsConfiguration.ALL);
+
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setMaxAge(3600L);
+
+        var corsConfigSource = new UrlBasedCorsConfigurationSource();
+        corsConfigSource.registerCorsConfiguration("/**", corsConfig);
+        return corsConfigSource;
     }
 }
