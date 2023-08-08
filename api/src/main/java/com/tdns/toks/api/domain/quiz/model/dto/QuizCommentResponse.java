fdc6d42b127e8917fdfa8d55842d35b438b73a14
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-08-08 15:23:06 +0900

    fix: json format

--- api/src/main/java/com/tdns/toks/api/domain/quiz/model/dto/QuizCommentResponse.java
+++ api/src/main/java/com/tdns/toks/api/domain/quiz/model/dto/QuizCommentResponse.java
@@ -1,5 +1,6 @@
 package com.tdns.toks.api.domain.quiz.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.tdns.toks.core.domain.quizcomment.entity.QuizComment;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,6 +18,7 @@ public class QuizCommentResponse {
     private Integer likeCount;
     private String nickname;
     private String content;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime createdAt;
 
     public static QuizCommentResponse from(QuizComment comment, String nickname, int likeCount) {
