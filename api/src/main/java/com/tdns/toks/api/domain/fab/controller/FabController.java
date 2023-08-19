4d9336c3223369d6a72425b59a6e0ae9b9093d35
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-08-19 21:15:31 +0900

    fix: swagger 내용 정리

--- api/src/main/java/com/tdns/toks/api/domain/fab/controller/FabController.java
+++ api/src/main/java/com/tdns/toks/api/domain/fab/controller/FabController.java
@@ -3,6 +3,7 @@ package com.tdns.toks.api.domain.fab.controller;
 import com.tdns.toks.api.domain.fab.service.FabService;
 import com.tdns.toks.core.common.model.dto.ResponseDto;
 import com.tdns.toks.core.domain.auth.model.AuthUser;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
@@ -12,13 +13,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "FAB 페이지 관련 데이터 제공", description = "FAB data")
+@Tag(name = "FAB 서비스", description = "FAB API")
 @RestController
 @RequestMapping(path = "/api/v1/fab", produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 public class FabController {
     private final FabService fabService;
 
+    @Operation(summary = "FAB 데이터 제공", description = "FAB 페이지에서 필요한 모든 데이터 일괄 제공")
     @GetMapping
     public ResponseEntity<?> getFab(
             AuthUser authUser,
