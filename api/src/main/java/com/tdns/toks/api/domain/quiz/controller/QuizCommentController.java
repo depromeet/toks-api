d3693df2be8639ed4a42f4c9d606f7d45d04bd49
sieun <sieun960513@naver.com>
2023-09-02 16:17:50 +0900

    :bug: ModelAttribute 매핑 문제 수정

--- api/src/main/java/com/tdns/toks/api/domain/quiz/controller/QuizCommentController.java
+++ api/src/main/java/com/tdns/toks/api/domain/quiz/controller/QuizCommentController.java
@@ -10,14 +10,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "퀴즈 댓글 관리", description = "QUIZ Comment API")
 @RestController
@@ -42,7 +35,7 @@ public class QuizCommentController {
     public ResponseEntity<?> insert(
             AuthUser authUser,
             @PathVariable Long quizId,
-            @ModelAttribute @RequestBody QuizCommentCreateRequest request
+            @RequestBody QuizCommentCreateRequest request
     ) {
         var response = quizCommentService.insert(authUser, quizId, request);
         return ResponseDto.created(response);
