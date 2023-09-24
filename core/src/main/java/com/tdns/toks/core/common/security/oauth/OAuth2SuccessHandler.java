61a0994b19975c03fa4f3174d011616159c70352
sieun <sieun960513@naver.com>
2023-09-24 14:34:52 +0900

    :wrench: redirect host change (local -> tokstudy.com/toks-main)

--- core/src/main/java/com/tdns/toks/core/common/security/oauth/OAuth2SuccessHandler.java
+++ core/src/main/java/com/tdns/toks/core/common/security/oauth/OAuth2SuccessHandler.java
@@ -31,8 +31,8 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
         var userDetailDTO = (UserDetailDTO) authentication.getPrincipal();
 
-//        String host = "https://tokstudy.com";
-        String host = local;
+        String host = "https://tokstudy.com/toks-main";
+//        String host = local;
 
         String referer = request.getHeader("Referer");
         if (referer != null && referer.contains("localhost")) {
