c277ddb934ebf623cc98015035c97ca140144205
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-10-08 10:47:09 +0900

    imp: AuthUserValidator 수정

--- core/src/main/java/com/tdns/toks/core/domain/auth/AuthUserValidator.java
+++ core/src/main/java/com/tdns/toks/core/domain/auth/AuthUserValidator.java
@@ -1,17 +1,30 @@
 package com.tdns.toks.core.domain.auth;
 
 import com.tdns.toks.core.domain.auth.model.AuthUser;
+import com.tdns.toks.core.domain.auth.model.AuthUserImpl;
+import com.tdns.toks.core.domain.user.type.UserRole;
+import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 
 public class AuthUserValidator {
+    @NonNull
     public static boolean isAuthenticated(@Nullable AuthUser authUser) {
         return authUser != null;
     }
 
+    @NonNull
     public static Long getUidOrElseDefault(@Nullable AuthUser authUser) {
         if (isAuthenticated(authUser)) {
             return authUser.getId();
         }
         return -1L;
+    }
+
+    @NonNull
+    public static AuthUser getOrElseDefault(@Nullable AuthUser authUser) {
+        if (isAuthenticated(authUser)) {
+            return authUser;
+        }
+        return new AuthUserImpl(-1L, UserRole.ANONYMOUS);
     }
 }
