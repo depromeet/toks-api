a09b9ed632b304411d5a2ed6ed6aa4992b8b28c6
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2024-02-27 02:35:08 +0900

    fix: bottom_banner ddl

--- scripts/sql/DDL.sql
+++ scripts/sql/DDL.sql
@@ -204,5 +204,7 @@ CREATE TABLE `bottom_banner`
     `image_url`   varchar(512) NOT NULL COMMENT '배너 이미지',
     `landing_url` varchar(512) NOT NULL COMMENT '랜딩 url',
     `is_active`   tinyint(1) NOT NULL COMMENT '활성화 여부',
+    `created_at`  datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '등록일시',
+    `updated_at`  datetime              DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일시',
     primary key (id)
 ) ENGINE = InnoDB AUTO_INCREMENT=200000 DEFAULT CHARSET=utf8mb4 COMMENT='바텀 배너';
